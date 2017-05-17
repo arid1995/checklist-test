3aabba3b199228ca5e478a4a6c4dc59de5246030
arid1995 <arid1995@mail.ru>
2017-05-17 15:55:58 +0300

    All tests are ready

--- page.py
+++ page.py
@@ -48,10 +48,13 @@ class SchedulePage(BasePage):
             return False
         return True
 
-    def hasPeriodChanged(self):
-        firstElement = WebDriverWait(self.driver, self.DEFAULT_WAIT_TIME).until(
+    def __waitForFirstElement(self):
+        return WebDriverWait(self.driver, self.DEFAULT_WAIT_TIME).until(
             EC.presence_of_element_located((By.XPATH, "//tr[@class='schedule-timetable__item']"))
         )
+
+    def hasPeriodChanged(self):
+        firstElement = self.__waitForFirstElement()
 
         firstDate = firstElement.get_attribute('data-date')
 
@@ -60,7 +63,7 @@ class SchedulePage(BasePage):
 
         try:
             WebDriverWait(self.driver, self.DEFAULT_WAIT_TIME).until(
-                EC.presence_of_element_located(By.XPATH, "//tr[@data-date<%d]" % int(firstDate))
+                EC.presence_of_element_located((By.XPATH, "//tr[@data-date<%d]" % int(firstDate)))
             )
         except TimeoutException:
             return False
@@ -68,10 +71,13 @@ class SchedulePage(BasePage):
         return True
 
     def __dropDownChecker(self, name1, name2, menuLocator, itemLocator):
-        dropdown = self.driver.find_element(menuLocator).find_element_by_class_name('nav-pills_dropdown__active__title')
+        self.__waitForFirstElement()
+
+        dropdown = self.driver.find_element(*menuLocator).find_element_by_class_name(
+            'nav-pills_dropdown__active__title')
 
         dropdown.click()
-        self.driver.find_element((By.XPATH, "//li/a[text()='%s']" % name1)).click()
+        self.driver.find_element(By.XPATH, "//li/a[text()='%s']" % name1).click()
 
         try:
             WebDriverWait(self.driver, self.DEFAULT_WAIT_TIME).until(
@@ -95,10 +101,69 @@ class SchedulePage(BasePage):
     def hasGroupChanged(self):
         return self.__dropDownChecker('BALinux-11', 'DevAppiOS-11',
                                       (By.XPATH, "//div[@class='schedule-filters__item schedule-filters__item_group']"),
-                                      lambda s: (By.XPATH, "//nobr[text()='%s'" % s))
+                                      lambda s: (By.XPATH, "//nobr[text()='%s']" % s))
 
     def hasDisciplineChanged(self):
         return self.__dropDownChecker('Разработка приложений на iOS ', 'Программирование на Python',
                                       (By.XPATH, "//div[@class='schedule-filters__item "
                                                  "schedule-filters__item_discipline']"),
                                       lambda s: (By.XPATH, "//td/strong/a[text()='%s']" % s))
+
+    def hasScrolled(self):
+        self.__waitForFirstElement()
+        firstPosition = self.driver.execute_script('window.scrollTo(document.body.scrollWidth, '
+                                                   'document.body.scrollHeight); '
+                                                   'return document.body.scrollHeight')
+        self.driver.find_element(By.XPATH, "//td[@class='calendar-month__body__item__day "
+                                           "calendar-month__body__item__day_active']").click()
+        time.sleep(0.4)
+        lastPosition = self.driver.execute_script('return window.pageYOffset')
+        if firstPosition != lastPosition:
+            return True
+        return False
+
+    def hasWentMobile(self):
+        self.driver.find_element(By.XPATH, "//a[text()='Мобильная версия']").click()
+        schedule = self.driver.find_element(By.XPATH, "//table[@class='schedule-timetable']")
+        if schedule.value_of_css_property('max-width') == '600px':
+            return True
+        return False
+
+    def hasEventChanged(self):
+        return self.__dropDownChecker('Лекция', 'Семинар',
+                                      (By.XPATH, "//div[@class='schedule-filters__item schedule-filters__item_type']"),
+                                      lambda s: (By.XPATH, "//p[contains(., '%s')]" % s))
+
+    def hasInfoPoppedUp(self):
+        self.driver.find_element(By.XPATH, "//a[@class='schedule-show-info icon-info-blue']").click()
+        return self.driver.find_element(By.XPATH, "//div[@class='modal modal-show-info jqm-init']").is_displayed()
+
+    def hasNavigatedToBlog(self):
+        self.driver.find_element(By.XPATH, "//a[@class='icon-blog']").click()
+        try:
+            WebDriverWait(self.driver, self.DEFAULT_WAIT_TIME).until(
+                EC.presence_of_element_located((By.XPATH, "//div[@class='blog-section']"))
+            )
+        except TimeoutException:
+            return False
+        return True
+
+    def hasSubjectInfoPoppedUp(self):
+        self.driver.find_element(By.XPATH, "//div[@class='schedule-item js-schedule-item']").click()
+        try:
+            WebDriverWait(self.driver, self.DEFAULT_WAIT_TIME).until(
+                EC.presence_of_element_located((By.XPATH, "//div[@class='qtip qtip-default schedule-item-popup "
+                                                          "qtip-pos-tl qtip-focus']"))
+            )
+        except TimeoutException:
+            return False
+        return True
+
+    def isOnlyTwoWeeks(self):
+        self.__waitForFirstElement()
+        lastElement = self.driver.find_elements(By.XPATH, "//tr[@class='schedule-timetable__item']")[-1:][0]
+
+        lastDate = int(lastElement.get_attribute('data-date'))
+        if lastDate > int(time.time() * 1000) + 1209600000:
+            return False
+        return True
--- tests.py
+++ tests.py
@@ -10,17 +10,38 @@ class TestSchedule(unittest.TestCase):
         self.schedule_page = page.SchedulePage(self.browser)
         self.schedule_page.signInAndNavigate()
 
-    #def test_open_schedule(self):
-    #   self.assertTrue(self.schedule_page.isOpened())
+    def test_open_schedule(self):
+        self.assertTrue(self.schedule_page.isOpened())
 
-    #def test_schedule_period(self):
-    #    self.assertTrue(self.schedule_page.hasPeriodChanged())
+    def test_changing_schedule_period(self):
+        self.assertTrue(self.schedule_page.hasPeriodChanged())
 
-    #def test_group_change(self):
-        #self.assertTrue(self.schedule_page.hasGroupChanged())
+    def test_group_change(self):
+        self.assertTrue(self.schedule_page.hasGroupChanged())
 
     def test_discipline_change(self):
         self.assertTrue(self.schedule_page.hasDisciplineChanged())
+
+    def test_calendar_scroll(self):
+        self.assertTrue(self.schedule_page.hasScrolled())
+
+    def test_mobile_version(self):
+        self.assertTrue(self.schedule_page.hasWentMobile())
+
+    def test_events_change(self):
+        self.assertTrue(self.schedule_page.hasEventChanged())
+
+    def test_info_popup(self):
+        self.assertTrue(self.schedule_page.hasInfoPoppedUp())
+
+    def test_blog_navigation(self):
+        self.assertTrue(self.schedule_page.hasNavigatedToBlog())
+
+    def test_subject_info_popup(self):
+        self.assertTrue(self.schedule_page.hasSubjectInfoPoppedUp())
+
+    def test_schedule_period(self):
+        self.assertTrue(self.schedule_page.isOnlyTwoWeeks())
 
     def tearDown(self):
         self.browser.close()
